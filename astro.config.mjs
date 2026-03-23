dfb35a3562ac47346c7a5ad76a2bea6eacd5fd2e
UtakataKyosui <fill.ayaextech@gmail.com>
2026-03-24 02:05:43 +0900

    feat: 前提思想の説明を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,18 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'UtakataKyosui’s EcoSystem',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'ガイド',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '概要と前提', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: 'リファレンス',
 					autogenerate: { directory: 'reference' },
 				},
 			],
